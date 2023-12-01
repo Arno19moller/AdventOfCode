273372fcace06c21740a7939a858a09e897fb216
Arno <arno19moller@gmail.com>
2023-12-01 10:26:28 +0200

    Moved files into folders and added .gitignore file

---
+++ AdventOfCode/AdventOfCode/2023/Day1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day1_2023
+    {
+        private List<int> total = new List<int>();
+
+        public int Execute()
+        {
+            var lines = System.IO.File.ReadAllLines(@"../../../input9.txt");
+            var rowCount = lines.Length;
+            int[] calibrations = new int[rowCount];
+
+			foreach (var line in lines.Select((value, i) => new { value, i }))
+            {
+                var firstNum = "";
+                var lastNum = "";
+                var indexToLookFrom = 0;
+                foreach (var charr in line.value.Select((value, i) => new { value, i }))
+                {
+                    var isNum = int.TryParse(charr.value.ToString(), out var num);
+
+					var substring = line.value.Substring(indexToLookFrom, charr.i - indexToLookFrom + 1);
+
+
+					if (substring.EndsWith("one"))
+                    {
+						if (firstNum == "")
+						{
+							firstNum = "1";
+						}
+						lastNum = "1";
+						indexToLookFrom = charr.i;
+					}
+                    else if (substring.Contains("two"))
+                    {
+						if (firstNum == "")
+						{
+							firstNum = "2";
+						}
+						lastNum = "2";
+						indexToLookFrom = charr.i;
+					}
+                    else if (substring.Contains("three"))
+                    {
+						if (firstNum == "")
+						{
+							firstNum = "3";
+						}
+						lastNum = "3";
+						indexToLookFrom = charr.i;
+					}
+                    else if (substring.Contains("four"))
+                    {
+						if (firstNum == "")
+						{
+							firstNum = "4";
+						}
+						lastNum = "4";
+						indexToLookFrom = charr.i;
+					}
+                    else if (substring.Contains("five"))
+                    {
+						if (firstNum == "")
+						{
+							firstNum = "5";
+						}
+						lastNum = "5";
+						indexToLookFrom = charr.i;
+					}
+                    else if (substring.Contains("six"))
+                    {
+						if (firstNum == "")
+						{
+							firstNum = "6";
+						}
+						lastNum = "6";
+						indexToLookFrom = charr.i;
+					}
+                    else if (substring.Contains("seven"))
+                    {
+						if (firstNum == "")
+						{
+							firstNum = "7";
+						}
+						lastNum = "7";
+						indexToLookFrom = charr.i;
+					}
+                    else if (substring.Contains("eight"))
+                    {
+						if (firstNum == "")
+						{
+							firstNum = "8";
+						}
+						lastNum = "8";
+						indexToLookFrom = charr.i;
+					}
+                    else if (substring.Contains("nine"))
+                    {
+						if (firstNum == "")
+						{
+							firstNum = "9";
+						}
+						lastNum = "9";
+						indexToLookFrom = charr.i;
+					}
+
+                    if (isNum)
+                    {
+                        if (firstNum == "")
+                        {
+                            firstNum = num.ToString();
+                        }
+                        lastNum = num.ToString();
+                    }
+                }
+                calibrations[line.i] = int.Parse(firstNum + lastNum);
+				//Console.WriteLine(calibrations[line.i]);
+
+			}
+
+            return calibrations.Sum();
+        }
+    }
+}
--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -1,12 +1,13 @@
 ﻿using AdventOfCode;
 
-var day1 = new Day1();
-var day2 = new Day2();
-var day3 = new Day3();
-var day4 = new Day4();
-var day5 = new Day5();
-var day6 = new Day6();
-var day7 = new Day7();
-var day8 = new Day8();
+//var day1 = new Day1();
+//var day2 = new Day2();
+//var day3 = new Day3();
+//var day4 = new Day4();
+//var day5 = new Day5();
+//var day6 = new Day6();
+//var day7 = new Day7();
+//var day8 = new Day8();
+var day1 = new Day1_2023();
 
-Console.WriteLine(day8.Execute());
+Console.WriteLine(day1.Execute());
